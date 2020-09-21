779678668da044ee3cab6087e0c7304305f8ce1b
myeadon2017 <62955849+myeadon2017@users.noreply.github.com>
2020-09-21 11:47:11 -0400

    almost done with blog

--- rcworldd/rcblog/forms.py
+++ rcworldd/rcblog/forms.py
@@ -1,14 +1,41 @@
 from django import forms
-from .models import Post
+from .models import Post, Category
+
+#choices = [('photography', 'photography'), ('movies', 'movies'), ('tv shows', 'tv shows'),
+ #('music', 'music'), ('news', 'news'), ('cooking', 'cooking'), ('poetry', 'poetry'),
+  #('video games', 'video games')]
+choices =  Category.objects.all().values_list('name','name')
+
+choice_list = []
+
+for item in choices:
+    choice_list.append(item)
+
 
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ('title', 'title_tag', 'author', 'body')
+        fields = ('title', 'title_tag', 'author', 'category', 'body', 'snippet', 'header_image')
 
-        widget {
-            'title': form.TextInput(attrs={'class': 'form-control'}),
-            'title_tag': form.TextInput(attrs={'class': 'form-control'}),
-            'author': form.Select(attrs={'class': 'form-control'}),
-            'body': form.Textarea(attrs={'class': 'form-control'}),
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'title_tag': forms.TextInput(attrs={'class': 'form-control'}),
+            'author': forms.TextInput(attrs={'class': 'form-control', 'value':'', 'id':'Raquel', 'type':'hidden'}),
+            #'author': forms.Select(attrs={'class': 'form-control'}),
+            'category': forms.Select(choices=choice_list, attrs={'class': 'form-control'}),
+            'body': forms.Textarea(attrs={'class': 'form-control'}),
+            'snippet': forms.Textarea(attrs={'class': 'form-control'}),
+        }
+
+class EditForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ('title', 'title_tag', 'body', 'snippet')
+
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'title_tag': forms.TextInput(attrs={'class': 'form-control'}),
+            #'author': forms.Select(attrs={'class': 'form-control'}),
+            'body': forms.Textarea(attrs={'class': 'form-control'}),
+            'snippet': forms.Textarea(attrs={'class': 'form-control'}),
         }
